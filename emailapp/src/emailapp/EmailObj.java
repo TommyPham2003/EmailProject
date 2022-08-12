6511b601d633ea841720d53d99d8698d366e947b
TommyPham2003 <jeroumgt@gmail.com>
2022-08-11 23:03:46 -0500

    set up getting the department
    Made the setter to get the department the user is in

--- emailapp/src/emailapp/EmailObj.java
+++ emailapp/src/emailapp/EmailObj.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class EmailObj {
 	
 	private String firstName;
@@ -13,9 +15,35 @@ public class EmailObj {
 	public EmailObj(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		System.out.println("New Email Created: " + this.firstName + " " + this.lastName);
+		
+		//set the department
+		department = setDepartment();
+		System.out.println("Department: " + department);
 	}
 	
-	//department
+	//department setter
+	//department can be changed to whatever is needed to fit the situation
+	private String setDepartment() {
+		System.out.println("Enter your department:");
+		System.out.println("1 for department");
+		System.out.println("2 for department");
+		System.out.println("3 for department");
+		
+		//make a scanner to read in the department
+		Scanner departmentNumber = new Scanner(System.in);
+		int num = departmentNumber.nextInt();
+		
+		if(num == 1) {
+			return "department";
+		}
+		else if(num == 1) {
+			return "department";
+		}
+		else {
+			return "department";
+		}
+	}
 	
 	//generate a password
 	
--- emailapp/src/emailapp/emailz.java
+++ emailapp/src/emailapp/emailz.java
@@ -2,6 +2,7 @@ package emailapp;
 
 public class emailz {
 	public static void main(String [] args) {
+		EmailObj email1 = new EmailObj("John", "Smith");
 		
 	}
 }
