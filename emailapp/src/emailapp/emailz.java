a889124f1a782ad816394f7564c59aaad1b5f0cc
TommyPham2003 <jeroumgt@gmail.com>
2022-08-11 23:26:18 -0500

    add a function to receive the employee's name

--- emailapp/src/emailapp/emailz.java
+++ emailapp/src/emailapp/emailz.java
@@ -1,8 +1,19 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class emailz {
 	public static void main(String [] args) {
-		EmailObj email1 = new EmailObj("John", "Smith");
+		//get the user first and last name
+		System.out.println("Enter your first name");
+		Scanner name = new Scanner(System.in);
+		String firstName = name.next();
+		
+		System.out.println("Enter your last name");
+		//Scanner last = new Scanner(System.in);
+		String lastName = name.next();
+		
+		EmailObj email1 = new EmailObj(firstName, lastName);
 		
 	}
 }
