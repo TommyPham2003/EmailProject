8d998665a0b40060641fe3deabd12ebeb717e560
TommyPham2003 <jeroumgt@gmail.com>
2022-08-11 22:44:52 -0500

    add in the private variables for the class

--- emailapp/src/emailapp/EmailObj.java
+++ emailapp/src/emailapp/EmailObj.java
@@ -1,5 +1,13 @@
 package emailapp;
 
 public class EmailObj {
-
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailCapacity;
+	private String newEmail;
+	
+	
 }
